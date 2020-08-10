8c11ad5abe63c015cefc15d07697241dabb3541a
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-08-10 10:19:37 +0200

    ex.11 finished with FS

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -34,10 +34,31 @@ function handleProgress() {
   progressBar.style.flexBasis = `${advanceRate}%`;
 }
 
+function progressBarUpdate(e) {
+  advanceTime = (event.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = parseFloat(advanceTime);
+}
+
+function fullScreen() {
+  video.requestFullscreen();
+}
 // Event listeners
 video.addEventListener('click', togglePlay); // allows to play/pause the vid if clicked anywhere on it
+
+
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', rangeUpdate));
 // ranges.forEach(range => range.addEventListener('mousemove', rangeUpdate));
-video.addEventListener('timeupdate', handleProgress);
+
+// le boolean mousedown stocke le fait que le clic souris soit enfoncé ou non.
+let mousedown = false;
+progress.addEventListener('click', progressBarUpdate);
+// L'event listener sur le mousemove sur la barre d'avancement ne se déclenche que si mousedown est true (sans cette cond° additionnelle, dès qu'on passe la souris sur la barre d'avancement (=mousemove) la fonction progressBarUpdate est appelée)
+// Comme la fonction progressBarUpdate a besoin de l'event, ce paramètre doit être passé dans l'arrow function ci-dessous
+progress.addEventListener('mousemove', (e) => mousedown && progressBarUpdate(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+fullscreen.addEventListener('click', fullScreen);
